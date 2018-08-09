424afb9cf7235960eaf131f80c4b8173306d4a72
Yusuf Can TÜRK <ycturk@yandex.com>
2018-08-09 15:44:50 +0300

    Update statikyapicimetod.cs

--- statikyapicimetod.cs
+++ statikyapicimetod.cs
@@ -9,6 +9,7 @@ namespace ConsoleApplication113
     class test
     {
         //Not Statik Yapıcı Sınıfla işlem yapıldığında sadece bir kez çalışır. Daha sonra bir daha çalışmaz. 
+        // Statik Yapıcı Her türlü işlemde çalışır. Yapıcı nesne türetildiğinde çalışır.
         public static int b; //  statik özellik
         public int a; // özellik 
         static test() // statik yapıcı -- Static Yapıcıya erişim belirteci konulmaz ama public olur.
@@ -24,6 +25,9 @@ namespace ConsoleApplication113
     {
         static void Main(string[] args)
         {
+            /// Eğer en başta nesne türetilseydi hem statik yapıcı hem yapıcı metod çalışırdı. 
+            /// test t1 = new test(3);
+            ///
             test.b = 5; //Statik Yapıcı Çalıştı.
             test.b = 10; // Artık Statik Yapıcı Çalışmaz.
             Console.WriteLine(test.b);
